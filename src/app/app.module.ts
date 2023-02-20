be09b836baf16135f95cdce92ff73d8c56aae1f6
Sarah Drasner <sarah.drasner@gmail.com>
2023-02-20 14:58:19 -0700

    finish screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ScreenComponent } from './screen/screen.component';
+
 
 @NgModule({
   declarations: [
